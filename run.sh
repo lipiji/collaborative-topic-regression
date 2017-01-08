53b220c5e13172a45f5fe3249d0e8aa202d68823
Piji <pagelee.sd@gmail.com>
2017-01-08 22:08:13 +0800

    fix ctr options

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 ./lda-c/lda est 0.1 10 ./lda-c/settings.txt ./example/mult.dat  random output 
-./ctr --directory ./output --user example/users.dat --item example/items.dat --theta_init output/final.gamma
+./ctr --directory ./output --user example/users.dat --item example/items.dat  \
+      --mult example/mult.dat --beta_init output/final.beta --theta_init output/final.gamma \
+      --theta_opt 1 --learning_rate 0.1
